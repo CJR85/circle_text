7dcd8cf63dcc40fc903685b912dac1237c665524
Calvin Roberts <roberts.calvin85@gmail.com>
2023-10-21 23:48:50 +0100

    Add mousemove function to text

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+let text = document.querySelector('.text');
+
+document.addEventListener('mousemove', function (e) {
+  text.style.left = e.pageX + 'px';
+  text.style.top = e.pageY + 'px';
+});
